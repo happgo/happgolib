18f8e100394d69069812cf9182780130b3c21dac
zhangbofa <626223818@qq.com>
2019-09-18 17:33:16 +0800

    init

---
+++ src/Collection.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by IntelliJ IDEA
+ * Author: 张伯发
+ * Date  : 2019/9/10
+ * Time  : 17:53
+ */
+
+namespace Happgo\Lib;
+
+
+class Collection
+{
+
+    public function hello()
+    {
+        var_dump('lib hello');
+
+    }
+
+}
---
+++ src/functions.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by IntelliJ IDEA
+ * Author: 张伯发
+ * Date  : 2019/9/10
+ * Time  : 17:52
+ */
